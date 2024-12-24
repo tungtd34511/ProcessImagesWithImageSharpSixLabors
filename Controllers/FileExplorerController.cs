42ce3fb62568a736124b9fbfd5ae630ba874931f
tungtd@T1 CON <taduytung24112000@gmail.com>
2024-12-24 08:52:25 +0700

    Add project files.

---
+++ Controllers/FileExplorerController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ProcessImagesWithImageSharpSixLabors.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class FileExplorerController : ControllerBase
+    {
+        [HttpGet("index")]
+        public IActionResult Index()
+        {
+            return Ok();
+        }
+    }
+}
